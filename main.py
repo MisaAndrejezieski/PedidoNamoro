f97aaca846147752dca1543a5e86389d2595dd35
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-11-16 02:57:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tkinter import messagebox
 root = tk.Tk()
 root.title('ACEITAS?')
 root.geometry('600x600')
-root.configure(background='#affc8dd')
+root.configure(background='#affc8d')
 
 def move_button_1(e):
     if abs(e.x - button_1.winfo_x()) < 50 and abs(e.y - button_1.winfo_y()) < 40:
@@ -22,10 +22,9 @@ def accepted():
 def denied():
     button_1.destroy()
     
-margin = Canvas(root, width=500, bg='#affc8dd', height=100, bd=0, highlightthickness=0, relief='ridge')    
-
+margin = Canvas(root, width=500, bg='#affc8d', height=100, bd=0, highlightthickness=0, relief='ridge')    
 margin.pack()
-text_id = Label(root, bg='#affc8dd', text='Quer namorar comigo?', fg='#500d22', font=('Montserrat', 24, 'bold'))
+text_id = Label(root, bg='#affc8d', text='Quer namorar comigo?', fg='#500d22', font=('Montserrat', 24, 'bold'))
 text_id.pack()
 button_1 = tk.Button(root, text='Não', bg='#affb3c1', command=denied, relief=RIDGE, bd=3, font=('Montserrat', 8, 'bold'))
 button_1.pack()
